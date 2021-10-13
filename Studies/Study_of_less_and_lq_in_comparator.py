6ce0a4442af5d5fb012196e1220cd818a7eb626c
Zexi.Li <lzx12138@sjtu.edu.cn>
2021-10-13 22:50:41 +0800

    Plot graphs for both `<` and `<=`.

--- Studies/Study_of_less_and_lq_in_comparator.py
+++ Studies/Study_of_less_and_lq_in_comparator.py
@@ -3,26 +3,29 @@ from Modules import Utils, LFSR
 if __name__ == '__main__':
     N = 8
     polynomial = [0, 1, 2, 5, 6]
-
     seed = [0 for _ in range(N)]
     seed[-1] = 1
+    LEN = 2**N
 
     nums = LFSR.simulate(N, seed, polynomial)
 
-    LEN = 2**N
     and_fn = lambda x, y: [1 if (x[i] == 1 and y[i] == 1) else 0 for i in range(len(x))]
 
-    y_sim = [0 for _ in range(1, LEN)]
-    for x in range(1, LEN):
-        bitstream_1 = Utils.comparator(nums, x, le=True)
+    MAE = 0
+    y_sim = [0 for _ in range(LEN)]
+    for x in range(LEN):
+        bitstream_1 = Utils.comparator(nums, x, le=False)
         bitstream_2 = Utils.DFF(bitstream_1, 4)
 
-        y_sim[x - 1] = Utils.count_arr(and_fn(bitstream_1, bitstream_2)) / (LEN - 1)
-        # y_sim[x - 1] = Utils.count_arr(and_fn(bitstream_1, bitstream_2))
+        y_sim[x] = Utils.count_arr(and_fn(bitstream_1, bitstream_2)) / (LEN - 1)
+
+        MAE += abs(y_sim[x] - x**2 / (LEN - 1)**2)
+
+    MAE /= LEN
 
     from matplotlib import pyplot as pl
 
-    x = range(1, LEN)
+    x = range(LEN)
     y_real = [i**2 / (LEN - 1)**2 for i in x]
     # y_real = [i**2 for i in x]
     pl.plot(x, y_real)
@@ -32,7 +35,33 @@ if __name__ == '__main__':
     print(y_real)
     print(y_sim)
 
+    print(MAE)
+
     # print(and_fn(x1_bitstream, x2_bitstream))
 
+    MAE = 0
+    y_sim = [0 for _ in range(LEN)]
+    for x in range(LEN):
+        bitstream_1 = Utils.comparator(nums, x, le=True)
+        bitstream_2 = Utils.DFF(bitstream_1, 4)
 
+        y_sim[x] = Utils.count_arr(and_fn(bitstream_1, bitstream_2)) / (LEN - 1)
+        # y_sim[x - 1] = Utils.count_arr(and_fn(bitstream_1, bitstream_2))
 
+        MAE += abs(y_sim[x] - x**2 / (LEN - 1)**2)
+
+    MAE /= (LEN - 1)
+
+    from matplotlib import pyplot as pl
+
+    x = range(LEN)
+    y_real = [i**2 / (LEN - 1)**2 for i in x]
+    # y_real = [i**2 for i in x]
+    pl.plot(x, y_real)
+    pl.plot(x, y_sim, '--')
+    pl.show()
+
+    print(y_real)
+    print(y_sim)
+
+    print(MAE)
