cba2f1b49983b523c840459ac15d52e2570c7ccf
Zexi.Li <lzx12138@sjtu.edu.cn>
2021-10-14 00:35:32 +0800

    Add Priority_queue, implemented by list.

---
+++ Modules/Data_structure.py
@@ -0,0 +1,21 @@
+
+class Priority_queue:
+    def __init__(self):
+        self.arr = list()
+
+    def add(self, element):
+        self.arr.append(element)
+        list.sort(self.arr, key=lambda a: a.val())
+
+    def remove_last(self):
+        del(self.arr[-1])
+
+    def length(self):
+        return len(self.arr)
+
+    def empty(self):
+        return len(self.arr) == 0
+
+    def print(self):
+        for i in self.arr:
+            print(i)
