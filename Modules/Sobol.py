1be7955e7f79c3b78ddf2bdbcea8251c7339938e
Zexi.Li <lzx12138@sjtu.edu.cn>
2021-10-14 13:58:42 +0800

    Fix bugs.

--- Modules/Sobol.py
+++ Modules/Sobol.py
@@ -3,17 +3,21 @@ import sobol
 
 
 def generate(dim: int, num: int):
-    sample = sobol.sample(dim, num)
+    sample = sobol.sample(dim, num - 1)
     # numpy.transpose(sample)
     # numpy.swapaxes(sample, 0, 1)
     # sample.transpose()
+    # sample = numpy.insert(sample, 0, 0, axis=0)
+
     sample = sample.T
+    sample *= num
+
 
     # return sample.tolist()
     return sample.tolist()
 
 
 if __name__ == '__main__':
-    arr_0, arr_1 = generate(2, 5)
-    print(type(arr_0))
+    arr_0, arr_1 = generate(2, 8)
+    print(arr_0)
     print(arr_1)
