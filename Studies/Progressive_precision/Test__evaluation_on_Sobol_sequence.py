1014f1ac896791a220971d46835e341aa7a7449d
Jacky Li <lzx12138@sjtu.edu.cn>
2021-10-20 22:06:35 +0800

    Update test on evaluation of sobol_sequence - generating multiple sequences.

--- Studies/Progressive_precision/Test__evaluation_on_Sobol_sequence.py
+++ Studies/Progressive_precision/Test__evaluation_on_Sobol_sequence.py
@@ -2,11 +2,12 @@ import Evaluate_function__point_distribution
 import Progress_core
 from Modules import Sobol
 if __name__ == '__main__':
+    sobol_num = 10
     N = 5
     LEN = 2**N
-    sobol_sequence = Sobol.generate(1, LEN)
-    sobol_sequence[0:0] = [0]
-    _, sobol_sequence_rotate = Progress_core.rotate(N, None, sobol_sequence)
-    ret = Evaluate_function__point_distribution.evaluate_2(N, sobol_sequence_rotate)
-    print(sobol_sequence)
-    print(ret)
+    sobol_sequences = Sobol.generate(sobol_num, LEN)
+    for sobol_sequence in sobol_sequences:
+        _, sobol_sequence_rotate = Progress_core.rotate(N, None, sobol_sequence)
+        ret = Evaluate_function__point_distribution.evaluate_2(N, sobol_sequence_rotate)
+        print(sobol_sequence)
+        print(ret)
