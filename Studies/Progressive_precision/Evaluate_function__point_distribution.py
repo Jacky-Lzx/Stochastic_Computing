76b98b512208589b003df373a94f778802a07bb0
Jacky Li <lzx12138@sjtu.edu.cn>
2021-10-20 17:02:41 +0800

    Add new evaluation function.

--- Studies/Progressive_precision/Evaluate_function__point_distribution.py
+++ Studies/Progressive_precision/Evaluate_function__point_distribution.py
@@ -32,9 +32,48 @@ def evaluate(n: int, nums: list) -> int:
         # print(ans)
     return ans
 
+def evaluate_2(n: int, nums: list) -> int:
+    ans = 0
+    # print(nums)
+    for i in range(max(1, n - 3), n):
+        count = [0 for _ in range(2**i)]
+        zeros = 2**i
+        for j in range(2**i):
+            # TODO: think twice here
+            if j == 2**n - 1:
+                break
+
+            index = math.ceil(nums[j] / 2**n * 2**i) - 1
+            if count[index] == 0:
+                zeros -= 1
+            count[index] += 1
+
+            assert zeros == sum([1 if a == 0 else 0 for a in count])
+        ans += zeros
+
+        for k in range(2**n - 1):
+            index_1 = math.ceil(nums[k] / 2**n * 2**i) - 1
+            index_2 = math.ceil(nums[k + 2**i] / 2**n * 2**i) - 1
+            if count[index_1] == 1:
+                count[index_1] = 0
+                zeros += 1
+            elif count[index_1] > 1:
+                count[index_1] -= 1
+
+            if count[index_2] == 0:
+                zeros -= 1
+            count[index_2] += 1
+
+            assert zeros == sum([1 if a == 0 else 0 for a in count])
+        ans += zeros
+
+    return ans
+
+
 
 if __name__ == '__main__':
-    N = 4
+
+    N = 6
     LEN = 2**N
     print(f"N = {N}")
     polynomials = LFSR.search_polynomials(N)
@@ -50,7 +89,7 @@ if __name__ == '__main__':
 
     priority_queue = Priority_queue()
 
-    # priority_queue = PriorityQueue()
+    import Progress_core
 
     for poly in polynomials:
         print("Now: {}".format(poly))
@@ -58,16 +97,19 @@ if __name__ == '__main__':
             for scram in scramblings:
                 num_sequence = LFSR.simulate(N, seed, poly, scram)
 
-                value = evaluate(N, num_sequence)
+                _, num_sequence_rotate = Progress_core.rotate(N, None, num_sequence)
 
-                if not priority_queue.empty() and value > priority_queue.last_value():
-                    continue
+                value = evaluate_2(N, num_sequence_rotate)
 
-                set = Setting(poly, seed, scram, value)
+                # if not priority_queue.empty() and value > priority_queue.last_value():
+                #     continue
 
-                priority_queue.add(set)
-                if priority_queue.length() > 100:
-                    priority_queue.remove_last()
+                set_t = Setting(poly, seed, scram, value)
 
+                priority_queue.add(set_t)
+                if priority_queue.length() > 200:
+                    priority_queue.sort()
+                    priority_queue.remove_half()
 
+    priority_queue.sort()
     priority_queue.print()
