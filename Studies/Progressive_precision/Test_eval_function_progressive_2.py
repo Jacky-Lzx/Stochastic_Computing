bbe01ede20f8793509c7d6031a001a6e130adf64
Jacky Li <lzx12138@sjtu.edu.cn>
2021-11-03 21:43:15 +0800

    Update for inserting zero.

--- Studies/Progressive_precision/Test_eval_function_progressive_2.py
+++ Studies/Progressive_precision/Test_eval_function_progressive_2.py
@@ -1,4 +1,5 @@
-from Modules import LFSR, Utils, Sobol
+from Modules import Utils, Sobol
+from Modules.LFSR import LFSR
 import Progress_core
 
 if __name__ == '__main__':
@@ -34,23 +35,24 @@ if __name__ == '__main__':
 
     LEN = 2**N
 
-    and_fn = lambda x, y: [1 if (x[i] == 1 and y[i] == 1) else 0 for i in range(len(x))]
-    real_fn = lambda x: x**2 / (LEN - 1)**2
+    def and_fn(x, y): return [1 if (x[i] == 1 and y[i] == 1) else 0 for i in range(len(x))]
+    def real_fn(x): return x**2 / LEN**2
 
-    nums_1 = LFSR.simulate(N, seed_1, poly_1, scram_1)
-    nums_2 = LFSR.simulate(N, seed_2, poly_2, scram_2)
-    new_1, new_2 = Progress_core.rotate(N, nums_1, nums_2)
+    a_LFSR = LFSR(N)
+    setting_1 = LFSR.setting(seed_1, poly_1, scram_1, None, True, 0)
+    nums_1 = a_LFSR.simulate(setting_1)
+    setting_2 = LFSR.setting(seed_2, poly_2, scram_2, None, True, 0)
+    nums_2 = a_LFSR.simulate(setting_2)
+    new_1, new_2 = Progress_core.rotate(nums_1, nums_2)
     MAEs_LFSR = Progress_core.simulate(N, new_1, new_2, and_fn, real_fn)
 
     N = 5
     LEN = 2**N
     sobol_nums_1, sobol_nums_2 = Sobol.generate(2, LEN)
-    sobol_new_1, sobol_new_2 = Progress_core.rotate(N, sobol_nums_1, sobol_nums_2)
+    sobol_new_1, sobol_new_2 = Progress_core.rotate(sobol_nums_1, sobol_nums_2)
     MAEs_Sobol = Progress_core.simulate(N, sobol_new_1, sobol_new_2, and_fn, real_fn)
 
-
     from matplotlib import pyplot as plt
-
     plt.plot(range(len(MAEs_LFSR)), MAEs_LFSR)
     plt.plot(range(len(MAEs_Sobol)), MAEs_Sobol)
     # plt.ylim((0, 0.08))
--- Studies/Progressive_precision/Test_eval_function_progressive_3.py
+++ Studies/Progressive_precision/Test_eval_function_progressive_3.py
@@ -1,6 +1,7 @@
 import random
 
-from Modules import LFSR, Utils, Sobol
+from Modules import Utils, Sobol
+from Modules.LFSR import LFSR
 import Progress_core
 
 if __name__ == '__main__':
@@ -41,19 +42,21 @@ if __name__ == '__main__':
     # poly_2 = [0, 1, 3, 4, 5]
     # seed_2 = [0, 0, 1, 0, 0, 0, 1]
     # scram_2 = (0, 1, 6, 2, 5, 3, 4)
-
     LEN = 2**N
 
-    and_fn = lambda x, y: [1 if (x[i] == 1 and y[i] == 1) else 0 for i in range(len(x))]
-    real_fn = lambda x: x**2 / (LEN - 1)**2
+    def and_fn(x, y): return [1 if (x[i] == 1 and y[i] == 1) else 0 for i in range(len(x))]
+    def real_fn(x): return x**2 / LEN**2
 
-    nums_1 = LFSR.simulate(N, seed_1, poly_1, scram_1)
-    nums_2 = LFSR.simulate(N, seed_2, poly_2, scram_2)
-    new_1, new_2 = Progress_core.rotate(N, nums_1, nums_2)
+    a_LFSR = LFSR(N)
+    setting_1 = LFSR.setting(seed_1, poly_1, scram_1, None, True, 0)
+    nums_1 = a_LFSR.simulate(setting_1)
+    setting_2 = LFSR.setting(seed_2, poly_2, scram_2, None, True, 0)
+    nums_2 = a_LFSR.simulate(setting_2)
+    new_1, new_2 = Progress_core.rotate(nums_1, nums_2)
     MAEs_LFSR = Progress_core.simulate(N, new_1, new_2, and_fn, real_fn)
 
     # Random LFSRs
-    polynomials = LFSR.search_polynomials(N)
+    polynomials = a_LFSR.search_polynomials()
     seeds = Utils.generate_seeding(N)
     scramblings = Utils.generate_scrambling(N)
     poly_rand_1 = polynomials[random.randint(0, len(polynomials) - 1)]
@@ -63,23 +66,22 @@ if __name__ == '__main__':
     scram_rand_1 = scramblings[random.randint(0, len(scramblings) - 1)]
     scram_rand_2 = scramblings[random.randint(0, len(scramblings) - 1)]
 
-    nums_rand_1 = LFSR.simulate(N, seed_rand_1, poly_rand_1, scram_rand_1)
-    nums_rand_2 = LFSR.simulate(N, seed_rand_2, poly_rand_2, scram_rand_2)
-    new_rand_1, new_rand_2 = Progress_core.rotate(N, nums_rand_1, nums_rand_2)
+    setting_3 = LFSR.setting(seed_rand_1, poly_rand_1, scram_rand_1, None, True, 0)
+    nums_rand_1 = a_LFSR.simulate(setting_3)
+    setting_4 = LFSR.setting(seed_rand_2, poly_rand_2, scram_rand_2, None, True, 0)
+    nums_rand_2 = a_LFSR.simulate(setting_4)
+    new_rand_1, new_rand_2 = Progress_core.rotate(nums_rand_1, nums_rand_2)
     MAEs_LFSR_rand = Progress_core.simulate(N, new_rand_1, new_rand_2, and_fn, real_fn)
-
 
     # Random Sobols
     random_times = 10
     sobols = Sobol.generate(random_times, LEN)
     sobol_nums_1 = sobols[random.randint(0, random_times - 1)]
     sobol_nums_2 = sobols[random.randint(0, random_times - 1)]
-    sobol_new_1, sobol_new_2 = Progress_core.rotate(N, sobol_nums_1, sobol_nums_2)
+    sobol_new_1, sobol_new_2 = Progress_core.rotate(sobol_nums_1, sobol_nums_2)
     MAEs_Sobol = Progress_core.simulate(N, sobol_new_1, sobol_new_2, and_fn, real_fn)
 
-
     from matplotlib import pyplot as plt
-
     plt.plot(range(len(MAEs_LFSR)), MAEs_LFSR)
     plt.plot(range(len(MAEs_Sobol)), MAEs_Sobol)
     plt.plot(range(len(MAEs_LFSR_rand)), MAEs_LFSR_rand)
@@ -88,5 +90,4 @@ if __name__ == '__main__':
     plt.xlabel("n")
     plt.ylabel("MAE")
     plt.show()
-
 
