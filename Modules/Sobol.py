9729312ecb5d7a4d0448ebb7c8008fb2cd7d6480
Jacky Li <lzx12138@sjtu.edu.cn>
2021-11-03 20:47:05 +0800

    Change Sobol generator to insert zero, and return [<sobol>] when num = 1.

--- Modules/Sobol.py
+++ Modules/Sobol.py
@@ -9,14 +9,13 @@ def generate(dim: int, num: int) -> List[List[int]]:
     # numpy.transpose(sample)
     # numpy.swapaxes(sample, 0, 1)
     # sample.transpose()
-    # sample = numpy.insert(sample, 0, 0, axis=0)
+    sample = numpy.insert(sample, 0, 0, axis=0)
 
     sample = sample.T
     sample *= num
 
-
     # return sample.tolist()
-    return sample.tolist()[0] if dim == 1 else sample.tolist()
+    return sample.tolist()
 
 
 if __name__ == '__main__':
