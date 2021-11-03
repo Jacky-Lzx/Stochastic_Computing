aef60575a09a27619237a8aa7ef1032f2eacdab1
Jacky Li <lzx12138@sjtu.edu.cn>
2021-11-03 20:48:39 +0800

    Update for new sobol generator.

--- Studies/Insert_zero/Sobol_property.py
+++ Studies/Insert_zero/Sobol_property.py
@@ -6,9 +6,9 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
     N = 8
     LEN = 2**N
-    sobol_nums = Sobol.generate(1, LEN)
+    sobol_nums = Sobol.generate(1, LEN)[0]
     sobol_nums = Utils.DFF(sobol_nums, 1)
     print(sobol_nums)
-    plt.plot(range(LEN - 1), sobol_nums, 'o')
+    plt.plot(range(LEN), sobol_nums, 'o')
     plt.show()
 
