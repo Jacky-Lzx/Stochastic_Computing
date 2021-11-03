33afbbfa869039921d939c3a6c5b0d7b2f408827
Jacky Li <lzx12138@sjtu.edu.cn>
2021-11-03 21:23:59 +0800

    Update for inserting zero.

--- Studies/Progressive_precision/Test_eval_function_progressive.py
+++ Studies/Progressive_precision/Test_eval_function_progressive.py
@@ -1,4 +1,5 @@
-from Modules import LFSR, Utils, Sobol
+from Modules import Utils, Sobol
+from Modules.LFSR import LFSR
 import Progress_core
 
 if __name__ == '__main__':
@@ -21,27 +22,31 @@ if __name__ == '__main__':
     scram_2 = (0, 1, 6, 2, 5, 3, 4)
 
     LEN = 2**N
+    a_LFSR = LFSR(N)
 
-    and_fn = lambda x, y: [1 if (x[i] == 1 and y[i] == 1) else 0 for i in range(len(x))]
-    real_fn = lambda x: x**2 / (LEN - 1)**2
+    def and_fn(x, y): return [1 if (x[i] == 1 and y[i] == 1) else 0 for i in range(len(x))]
+    def real_fn(x): return x**2 / LEN**2
 
-    nums_1 = LFSR.simulate(N, seed_1, poly_1, scram_1)
-    nums_2 = LFSR.simulate(N, seed_2, poly_2, scram_2)
-    new_1, new_2 = Progress_core.rotate(N, nums_1, nums_2)
-    MAEs_LFSR = Progress_core.simulate(N, new_1, new_2, and_fn, real_fn)
+    setting_1 = LFSR.setting(seed_1, poly_1, scram_1, None, True, 0)
+    nums_1 = a_LFSR.simulate(setting_1)
+    print(f"length of nums_1: {len(nums_1)}")
+    setting_2 = LFSR.setting(seed_2, poly_2, scram_2, None, True, 0)
+    nums_2 = a_LFSR.simulate(setting_2)
+    print(f"length of nums_2: {len(nums_2)}")
+    new_1, new_2 = Progress_core.rotate(nums_1, nums_2)
+    print(f"length of new_1: {len(new_1)}")
+    MAEs_LFSR = Progress_core.simulate(N, new_1, new_2, and_fn, real_fn, le=False)
 
     N = 6
     LEN = 2**N
     sobol_nums_1, sobol_nums_2 = Sobol.generate(2, LEN)
     sobol_new_1, sobol_new_2 = Progress_core.rotate(sobol_nums_1, sobol_nums_2)
-    MAEs_Sobol = Progress_core.simulate(N, sobol_new_1, sobol_new_2, and_fn, real_fn)
-
+    MAEs_Sobol = Progress_core.simulate(N, sobol_new_1, sobol_new_2, and_fn, real_fn, le=False)
 
     from matplotlib import pyplot as plt
-
     plt.plot(range(len(MAEs_LFSR[1:])), MAEs_LFSR[1:])
     plt.plot(range(len(MAEs_Sobol)), MAEs_Sobol)
-    plt.ylim((0, 0.08))
+    # plt.ylim((0, 0.08))
     plt.legend(['7-bit LFSR', '6-bit Sobol'])
     plt.xlabel("n")
     plt.ylabel("MAE")
