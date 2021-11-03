06b399273d754175d08500d8a3cf189a10bb6c04
Jacky Li <lzx12138@sjtu.edu.cn>
2021-11-03 21:43:31 +0800

    Update for new `rotate()`.

--- Studies/Progressive_precision/Test__evaluation_on_Sobol_sequence.py
+++ Studies/Progressive_precision/Test__evaluation_on_Sobol_sequence.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     LEN = 2**N
     sobol_sequences = Sobol.generate(sobol_num, LEN)
     for sobol_sequence in sobol_sequences:
-        _, sobol_sequence_rotate = Progress_core.rotate(N, None, sobol_sequence)
+        _, sobol_sequence_rotate = Progress_core.rotate(None, sobol_sequence)
         ret = Evaluate_function__point_distribution.evaluate_3(N, sobol_sequence_rotate)
         print(sobol_sequence)
         print(ret)
