335779a18cc0bac67b8bd18c614c4684c8a1b77c
Jacky Li <lzx12138@sjtu.edu.cn>
2021-11-04 14:52:55 +0800

    Change for random in good LFSRs.

--- Studies/Progressive_precision/Test_eval_function_progressive_3.py
+++ Studies/Progressive_precision/Test_eval_function_progressive_3.py
@@ -6,54 +6,55 @@ import Progress_core
 
 if __name__ == '__main__':
     # N = 4
-    # seed_1 = [0, 1, 0, 1]
-    # poly_1 = [0]
-    # scram_1 = (3, 0, 1, 2)
+
+    N = 5
+    # poly_1 = [1]
+    # seed_1 = [1, 1, 1, 0, 1]
+    # scram_1 = (0, 1, 4, 2, 3)
     #
-    # seed_2 = [0, 1, 0, 1]
     # poly_2 = [2]
-    # scram_2 = (3, 0, 2, 1)
+    # seed_2 = [1, 0, 1, 1, 1]
+    # scram_2 = (4, 3, 0, 1, 2)
 
-    N = 6
 
-    # poly_1 = [4]
-    # seed_1 = [0, 1, 0, 0, 0, 1]
-    # scram_1 = (2, 0, 3, 5, 1, 4)
-    # poly_1 = [4]
-    # seed_1 = [0, 1, 0, 0, 0, 1]
-    # scram_1 = (2, 0, 3, 5, 1, 4)
-    poly_1 = [4]
-    seed_1 = [0, 1, 0, 0, 0, 1]
-    scram_1 = (2, 0, 3, 5, 1, 4)
+    file_polys = list()
+    file_seeds = list()
+    file_scrams = list()
+    with open("Evaluate_function_3_result_n=5_inserting_zero.txt") as in_file:
+        lines = in_file.readlines()
+        for line in lines:
+            if line.startswith("poly"):
+                # file_polys.append(line[5:-1])
+                file_polys.append([int(a) for a in line[7:-2].split(", ")])
+            elif line.startswith("seed"):
+                file_seeds.append([int(a) for a in line[7:-2].split(", ")])
+            elif line.startswith("scram"):
+                file_scrams.append(tuple([int(a) for a in line[8:-2].split(", ")]))
 
-    poly_2 = [4]
-    seed_2 = [0, 1, 0, 0, 0, 1]
-    scram_2 = (2, 0, 3, 5, 4, 1)
-
-    # poly_2 = [0, 3, 4]
-    # seed_2 = [1, 1, 1, 1, 0, 0]
-    # scram_2 = (1, 3, 4, 0, 2, 5)
-
-    # N = 7
-    # poly_1 = [0, 3, 5]
-    # seed_1 = [0, 0, 0, 0, 1, 1, 1]
-    # scram_1 = (4, 1, 6, 3, 2, 0, 5)
-    #
-    # poly_2 = [0, 1, 3, 4, 5]
-    # seed_2 = [0, 0, 1, 0, 0, 0, 1]
-    # scram_2 = (0, 1, 6, 2, 5, 3, 4)
     LEN = 2**N
 
     def and_fn(x, y): return [1 if (x[i] == 1 and y[i] == 1) else 0 for i in range(len(x))]
     def real_fn(x): return x**2 / LEN**2
 
     a_LFSR = LFSR(N)
-    setting_1 = LFSR.setting(seed_1, poly_1, scram_1, None, True, 0)
-    nums_1 = a_LFSR.simulate(setting_1)
-    setting_2 = LFSR.setting(seed_2, poly_2, scram_2, None, True, 0)
-    nums_2 = a_LFSR.simulate(setting_2)
-    new_1, new_2 = Progress_core.rotate(nums_1, nums_2)
-    MAEs_LFSR = Progress_core.simulate(N, new_1, new_2, and_fn, real_fn)
+    for i in range(10):
+        rand_1 = 0
+        rand_2 = 0
+        while file_polys[rand_1] == file_polys[rand_2]:
+            rand_1 = random.randint(0, len(file_seeds) - 1)
+            rand_2 = random.randint(0, len(file_seeds) - 1)
+
+        seed_1, seed_2 = file_seeds[rand_1], file_seeds[rand_2]
+        poly_1, poly_2 = file_polys[rand_1], file_polys[rand_2]
+        scram_1, scram_2 = file_scrams[rand_1], file_scrams[rand_2]
+
+        setting_1 = LFSR.setting(seed_1, poly_1, scram_1, None, True, 0)
+        nums_1 = a_LFSR.simulate(setting_1)
+        setting_2 = LFSR.setting(seed_2, poly_2, scram_2, None, True, 0)
+        nums_2 = a_LFSR.simulate(setting_2)
+        new_1, new_2 = Progress_core.rotate(nums_1, nums_2)
+        MAEs_LFSR = Progress_core.simulate(N, new_1, new_2, and_fn, real_fn)
+        print(f"{i}'s random: {MAEs_LFSR}")
 
     # Random LFSRs
     polynomials = a_LFSR.search_polynomials()
@@ -72,6 +73,7 @@ if __name__ == '__main__':
     nums_rand_2 = a_LFSR.simulate(setting_4)
     new_rand_1, new_rand_2 = Progress_core.rotate(nums_rand_1, nums_rand_2)
     MAEs_LFSR_rand = Progress_core.simulate(N, new_rand_1, new_rand_2, and_fn, real_fn)
+    print(f"Random LFSR: {MAEs_LFSR_rand}")
 
     # Random Sobols
     random_times = 10
@@ -80,6 +82,8 @@ if __name__ == '__main__':
     sobol_nums_2 = sobols[random.randint(0, random_times - 1)]
     sobol_new_1, sobol_new_2 = Progress_core.rotate(sobol_nums_1, sobol_nums_2)
     MAEs_Sobol = Progress_core.simulate(N, sobol_new_1, sobol_new_2, and_fn, real_fn)
+
+    print(f"Sobol: {MAEs_Sobol}")
 
     from matplotlib import pyplot as plt
     plt.plot(range(len(MAEs_LFSR)), MAEs_LFSR)
